e328e0916ac13fc8379de648c6a7c3687d2ae9e3
Nick Meinhold <nick.meinhold@gmail.com>
2021-11-25 16:31:34 +1100

    try to cheat again

--- challenges/1/lib/main.dart
+++ challenges/1/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 // Flutter is Google's UI toolkit for building beautiful, natively compiled applications for mobile
-const flutterBlurb =
-    ", web, desktop, and embedded devices from a single codebase.";
+const flutterBlurb = "from a single codebase.";
 const challengeTitle = 'Challenge 1';
 void main() {
   runApp(const MyApp());
